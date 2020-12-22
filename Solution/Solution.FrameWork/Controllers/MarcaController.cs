70b6f78f483bc45a895adca1d6d4ec3403f511a2
Acruvelier <acruveliera1991@outlook.com>
2020-12-22 00:38:39 -0600

    Primer Avance
    Se agregó carpeta ViewModel con la clase Marca, Controlador para Marca y Vista para marca

---
+++ Solution/Solution.FrameWork/Controllers/MarcaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Solution.FrameWork.Models;
+using Solution.FrameWork.Models.ViewModels;
+
+namespace Solution.FrameWork.Controllers
+{
+    public class MarcaController : Controller
+    {
+        // GET: Marca
+        public ActionResult Index()
+        {
+            List<MarcaCLS> listaMarca = null;
+            //Creamos la conexión con la base de datos
+            using (var bd = new BDPasajeEntities())
+            {
+                listaMarca = (from marca in bd.Marca
+                              select new MarcaCLS
+                              {
+                                  //ClaseCLS = model.Dato
+                                  iddmarca = marca.IIDMARCA,
+                                  nombre = marca.NOMBRE,
+                                  descripcion = marca.DESCRIPCION
+                              }).ToList();
+            }
+            return View(listaMarca);
+        }
+    }
+}
---
+++ Solution/Solution.FrameWork/Models/ViewModels/MarcaCLS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Solution.FrameWork.Models.ViewModels
+{
+    public class MarcaCLS
+    {
+        [Display(Name = "Id Marca")]
+        public int iddmarca { get; set; }
+        [Display(Name = "Nombre Marca")]
+        public string nombre { get; set; }
+        [Display(Name = "Descripción Marca")]
+        public string descripcion { get; set; }
+        public  int bhabilitado { get; set; }
+    }
+}
